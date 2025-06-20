18534cab421cace4f96da2407f4a40d703da8d6a
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-06-20 16:50:50 +0100

    model w focal loss

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ eval "$(conda shell.bash hook)"
 conda activate rm_dermo_env
 
 # Run the training and evaluation scripts
+echo "=== TRAINING ==="
 python train.py
+echo "=== EVALUATION ==="
 python evaluate.py
