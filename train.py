1055213694fbfeb7a465169d05bf9c8fdc1949e2
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-06-28 17:24:32 +0100

    train mod

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ model.compile(
 # === TRAINING ===
 print("Training full model with top layers unfrozen...")
 callbacks = [
-    EarlyStopping(monitor="val_auc", mode="max", patience=100, restore_best_weights=True),
+    EarlyStopping(monitor="val_auc", mode="max", patience=50, restore_best_weights=True),
     ModelCheckpoint(MODEL_PATH, monitor="val_auc", mode="max", save_best_only=True, save_weights_only=True),
     ReduceLROnPlateau(monitor="val_auc", mode="max", factor=0.5, patience=7, min_lr=1e-7, verbose=1),
     RecallLogger()
