05f0a0c4a8042a231f0f8a87120a9976cac7c99d
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-06-28 14:22:52 +0100

    data_loader

--- data_loader.py
+++ data_loader.py
@@ -2,37 +2,35 @@ import os
 import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def load_dataframes(base_path):
+BASE_PATH = "/raid/DATASETS/rmiguel_datasets/ISIC16/Classification/Split"
+
+train_folder = os.path.join(BASE_PATH, "train")
+val_folder = os.path.join(BASE_PATH, "val")
+test_folder = os.path.join(BASE_PATH, "test")
+
+
+def load_dataframes(file_path):
 
     # Load CSV files
-    df_train = pd.read_csv(os.path.join(base_path, "train/train_labels.csv"), header=None, names=['image', 'label'])
-    df_val = pd.read_csv(os.path.join(base_path, "val/val_labels.csv"), header=None, names=['image', 'label'])
-    df_test = pd.read_csv(os.path.join(base_path, "test/val_labels.csv"), header=None, names=['image', 'label'])
+    df = pd.read_csv(os.path.join(BASE_PATH, file_path), header=None, names=['image', 'label'])
 
     # Convert labels to string format '0' and '1'
-    df_train['label'] = df_train['label'].astype(str)
-    df_val['label'] = df_val['label'].astype(str)
-    df_test['label'] = df_test['label'].astype(str)
+    df['label'] = df['label'].astype(str)
 
-    return df_train, df_val, df_test
+    return df
 
 
-def get_generators(train_csv_name=None, img_size=224, batch_size=64):
-    """
-    Generates training, validation, and testing generators using ImageDataGenerator.
-    Uses fixed split folders to prevent leakage.
-    """
-    train_df, val_df, test_df = load_dataframes(train_csv_name)
+def get_generators(img_size, batch_size):
 
-    # Directories
-    base_dir = "/raid/DATASETS/rmiguel_datasets/ISIC16/Classification/Split"
-    test_dir = "/raid/DATASETS/rmiguel_datasets/ISIC16/Preprocessed_Testing_Data"
+    train_df = load_dataframes(os.path.join(train_folder, "train_labels.csv"))
+    val_df = load_dataframes(os.path.join(train_folder, "val_labels.csv"))
+    test_df = load_dataframes(os.path.join(train_folder, "test_labels.csv"))
 
     datagen = ImageDataGenerator(rescale=1.0 / 255)
 
     train_gen = datagen.flow_from_dataframe(
         dataframe=train_df,
-        directory=os.path.join(base_dir, "train"),
+        directory=train_folder,
         x_col="image",
         y_col="label",
         target_size=(img_size, img_size),
@@ -44,7 +42,7 @@ def get_generators(train_csv_name=None, img_size=224, batch_size=64):
 
     val_gen = datagen.flow_from_dataframe(
         dataframe=val_df,
-        directory=os.path.join(base_dir, "val"),
+        directory=val_folder,
         x_col="image",
         y_col="label",
         target_size=(img_size, img_size),
@@ -55,7 +53,7 @@ def get_generators(train_csv_name=None, img_size=224, batch_size=64):
 
     test_gen = datagen.flow_from_dataframe(
         dataframe=test_df,
-        directory=test_dir,
+        directory=test_folder,
         x_col="image",
         y_col="label",
         target_size=(img_size, img_size),
