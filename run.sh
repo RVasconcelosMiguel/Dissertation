0878e672b5c1de0fba181ef8e50d5f407bc2db37
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-06-28 16:44:46 +0100

    train mod

--- run.sh
+++ run.sh
@@ -4,8 +4,20 @@
 eval "$(conda shell.bash hook)"
 conda activate rm_dermo_env
 
+
+
+#Initialization
+echo "=== Preprocess ==="
+python 2_preprocess.py
+
+echo "=== Augmentation ==="
+python 3_augmentation.py
+
+
+
 # Run the training and evaluation scripts
 echo "=== TRAINING ==="
 python train.py
+
 echo "=== EVALUATION ==="
 python evaluate.py
