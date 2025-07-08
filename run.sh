81ee146d451403134361b6317a3181c6f1e37763
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-08 17:24:26 +0100

    fine train mod

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ conda activate rm_dermo_env
 
 # Run the FINE training and evaluation scripts
 echo "=== TRAINING FINE ==="
-python train_fine.py | tee ../files_to_transfer/efficientnetb4/fine/train_fine_log.txt
+#python train_fine.py | tee ../files_to_transfer/efficientnetb4/fine/train_fine_log.txt
 
 echo "=== EVALUATION HEAD ==="
-#python evaluate_fine.py | tee ../files_to_transfer/efficientnetb4/fine/evaluate_fine_log.txt
+python evaluate_fine.py | tee ../files_to_transfer/efficientnetb4/fine/evaluate_fine_log.txt
