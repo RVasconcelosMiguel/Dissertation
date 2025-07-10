473729cf88e259aef29e038332121b1bdbdf1519
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-10 11:59:10 +0100

    fine train mod - checkpoint com lr scheduler mudado

--- train_fine.py
+++ train_fine.py
@@ -29,16 +29,16 @@ model_name = "efficientnetb4"
 IMG_SIZE = 380
 BATCH_SIZE = 16
 FINE_TUNE_EPOCHS = 60
-FINE_TUNE_LR = 5e-5
-LABEL_SMOOTHING_F = 0.001
+FINE_TUNE_LR = 3e-5
+LABEL_SMOOTHING_F = 0.01
 
 DROPOUT_H = 0.6   # keep consistent with head
 L2_REG_H = 1e-3
-DROPOUT_F = 0.2   # introduce base dropout in fine-tuning
-L2_REG_F = 1e-4
+DROPOUT_F = 0.3   # introduce base dropout in fine-tuning
+L2_REG_F = 1e-5
 
 THRESHOLD = 0.5
-CLASS_WEIGHTS_MULT_FINE = 2
+CLASS_WEIGHTS_MULT_FINE = 1.75
 
 # === PATHS ===
 output_dir = f"/home/jtstudents/rmiguel/files_to_transfer/{model_name}/fine"
@@ -139,7 +139,7 @@ print(f"[INFO] Unfroze the last {num_unfreeze} BatchNormalization layers for ada
 lr_schedule = ExponentialDecay(
     initial_learning_rate=FINE_TUNE_LR,
     decay_steps=100,
-    decay_rate=0.9,
+    decay_rate=0.96,
     staircase=True
 )
 
