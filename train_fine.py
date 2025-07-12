4ce51cd717e247bbcd8fd8535ccce138cb2a4303
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-12 11:56:52 +0100

    fine mode smoothing-0, dropout.0.2, LR 0.96

--- train_fine.py
+++ train_fine.py
@@ -34,7 +34,7 @@ LABEL_SMOOTHING_F = 0
 
 DROPOUT_H = 0.6   # keep consistent with head
 L2_REG_H = 1e-3
-DROPOUT_F = 0.4   # introduce base dropout in fine-tuning
+DROPOUT_F = 0.2   # introduce base dropout in fine-tuning
 L2_REG_F = 1e-5
 
 THRESHOLD = 0.5
@@ -139,7 +139,7 @@ print(f"[INFO] Unfroze the last {num_unfreeze} BatchNormalization layers for ada
 lr_schedule = ExponentialDecay(
     initial_learning_rate=FINE_TUNE_LR,
     decay_steps=60,
-    decay_rate=0.95,
+    decay_rate=0.96,
     staircase=True
 )
 
