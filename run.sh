c23a8d7406a9991a8a162cd0d531730b28f74bb4
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-14 00:59:39 +0100

    reproducibility

--- run.sh
+++ run.sh
@@ -10,24 +10,24 @@ conda activate rm_dermo_env
 #echo "=== Preprocess ==="
 #python 2_preprocess.py | tee ../files_to_transfer/preprocess_log.txt
 
-echo "=== Augmentation ==="
-python 3_augmentation.py | tee ../files_to_transfer/aug_log.txt
+#echo "=== Augmentation ==="
+#python 3_augmentation.py | tee ../files_to_transfer/aug_log.txt
 
 
 
 # Run the HEAD training and evaluation scripts
 
-#echo "=== TRAINING HEAD ==="
-#python train_head.py | tee ../files_to_transfer/efficientnetb4/head/train_head_log.txt
+echo "=== TRAINING HEAD ==="
+python train_head.py | tee ../files_to_transfer/efficientnetb4/head/train_head_log.txt
 
-#echo "=== EVALUATION HEAD ==="
-#python evaluate_head.py | tee ../files_to_transfer/efficientnetb4/head/evaluate_head_log.txt
+echo "=== EVALUATION HEAD ==="
+python evaluate_head.py | tee ../files_to_transfer/efficientnetb4/head/evaluate_head_log.txt
 
 
 
 # Run the FINE training and evaluation scripts
-#echo "=== TRAINING FINE ==="
-#python train_fine.py | tee ../files_to_transfer/efficientnetb4/fine/train_fine_log.txt
+echo "=== TRAINING FINE ==="
+python train_fine.py | tee ../files_to_transfer/efficientnetb4/fine/train_fine_log.txt
 
-#echo "=== EVALUATION HEAD ==="
-#python evaluate_fine.py | tee ../files_to_transfer/efficientnetb4/fine/evaluate_fine_log.txt
+echo "=== EVALUATION HEAD ==="
+python evaluate_fine.py | tee ../files_to_transfer/efficientnetb4/fine/evaluate_fine_log.txt
