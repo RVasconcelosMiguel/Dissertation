0fed854c5c4b5a97baa80782cd453f09508657d3
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-14 18:10:23 +0100

    No mask test

--- run.sh
+++ run.sh
@@ -11,20 +11,20 @@ python dir_creator.py
 #python 1_dataset_load.py | tee outputs/load_log.txt
 
 #Initialization
-#echo "=== Preprocess ==="
-#python 2_preprocess.py | tee outputs/preprocess_log.txt
+echo "=== Preprocess ==="
+python 2_preprocess.py | tee outputs/preprocess_log.txt
 
-#echo "=== Augmentation ==="
-#python 3_augmentation.py | tee outputs/aug_log.txt
+echo "=== Augmentation ==="
+python 3_augmentation.py | tee outputs/aug_log.txt
 
 
 
 # Run the HEAD training and evaluation scripts
-#echo "=== TRAINING HEAD ==="
-#python train_head.py | tee outputs/head/results/train_head_log.txt
+echo "=== TRAINING HEAD ==="
+python train_head.py | tee outputs/head/results/train_head_log.txt
 
-#echo "=== EVALUATION HEAD ==="
-#python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
+echo "=== EVALUATION HEAD ==="
+python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
 
 
 
@@ -33,7 +33,7 @@ echo "=== TRAINING FINE ==="
 python train_fine.py | tee outputs/fine/results/train_fine_log.txt
 
 echo "=== POSTPROCESSING ==="
-python postprocessing.py | tee outputs/fine/results/postprocessing_log.txt
+python postprocessing.py | tee outputs/postprocessing_log.txt
 
 echo "=== EVALUATION FINE ==="
 python evaluate_fine.py | tee outputs/fine/results/evaluate_fine_log.txt
