575a72e39189580e59a8c6ea7c7251b2a6d7c706
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-14 10:31:21 +0100

    directories reset

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ import os
 
 # Create output folder
 current_dir = os.path.dirname(os.path.abspath(__file__))
-outputs_dir = os.path.join(current_dir, "outputs")
-os.makedirs(outputs_dir, exist_ok=True)
+outputs_dir_1 = os.path.join(current_dir, "outputs/head")
+os.makedirs(outputs_dir_1, exist_ok=True)
+outputs_dir_2 = os.path.join(current_dir, "outputs/fine")
+os.makedirs(outputs_dir_2, exist_ok=True)
