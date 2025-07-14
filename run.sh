a75a75b532bb2e30ea0906ec5b6dfc742cc2d183
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-14 10:11:44 +0100

    directories reset

--- run.sh
+++ run.sh
@@ -5,26 +5,26 @@ eval "$(conda shell.bash hook)"
 conda activate rm_dermo_env
 
 
+
 #Data Load
 #echo "=== Data Load ==="
 #python 1_dataset_load.py | tee outputs/load_log.txt
 
 #Initialization
-#echo "=== Preprocess ==="
-#python 2_preprocess.py | tee outputs/preprocess_log.txt
+echo "=== Preprocess ==="
+python 2_preprocess.py | tee outputs/preprocess_log.txt
 
-#echo "=== Augmentation ==="
-#python 3_augmentation.py | tee outputs/aug_log.txt
+echo "=== Augmentation ==="
+python 3_augmentation.py | tee outputs/aug_log.txt
 
 
 
 # Run the HEAD training and evaluation scripts
+echo "=== TRAINING HEAD ==="
+python train_head.py | tee outputs/head/train_head_log.txt
 
-#echo "=== TRAINING HEAD ==="
-#python train_head.py | tee outputs/head/train_head_log.txt
-
-#echo "=== EVALUATION HEAD ==="
-#python evaluate_head.py | tee outputs/head/evaluate_head_log.txt
+echo "=== EVALUATION HEAD ==="
+python evaluate_head.py | tee outputs/head/evaluate_head_log.txt
 
 
 
