f5b342b62547805156533e6e2a1e98890429ec08
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-14 15:34:00 +0100

    directories reset

--- run.sh
+++ run.sh
@@ -11,20 +11,20 @@ python dir_creator.py
 #python 1_dataset_load.py | tee outputs/load_log.txt
 
 #Initialization
-#echo "=== Preprocess ==="
-#python 2_preprocess.py | tee outputs/preprocess_log.txt
+echo "=== Preprocess ==="
+python 2_preprocess.py | tee outputs/preprocess_log.txt
 
-#echo "=== Augmentation ==="
-#python 3_augmentation.py | tee outputs/aug_log.txt
+echo "=== Augmentation ==="
+python 3_augmentation.py | tee outputs/aug_log.txt
 
 
 
 # Run the HEAD training and evaluation scripts
-#echo "=== TRAINING HEAD ==="
-#python train_head.py | tee outputs/head/results/train_head_log.txt
+echo "=== TRAINING HEAD ==="
+python train_head.py | tee outputs/head/results/train_head_log.txt
 
-#echo "=== EVALUATION HEAD ==="
-#python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
+echo "=== EVALUATION HEAD ==="
+python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
 
 
 
