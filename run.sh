9b0bf707d9bc9432f8237b872dc846ec76a05e79
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-15 14:06:37 +0100

    AP

--- run.sh
+++ run.sh
@@ -14,26 +14,26 @@ python dir_creator.py
 echo "=== Preprocess ==="
 python 2_preprocess.py | tee outputs/preprocess_log.txt
 
-echo "=== Augmentation ==="
-python 3_augmentation.py | tee outputs/aug_log.txt
+#echo "=== Augmentation ==="
+#python 3_augmentation.py | tee outputs/aug_log.txt
 
 
 
 # Run the HEAD training and evaluation scripts
-echo "=== TRAINING HEAD ==="
-python train_head.py | tee outputs/head/results/train_head_log.txt
+#echo "=== TRAINING HEAD ==="
+#python train_head.py | tee outputs/head/results/train_head_log.txt
 
-echo "=== EVALUATION HEAD ==="
-python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
+#echo "=== EVALUATION HEAD ==="
+#python evaluate_head.py | tee outputs/head/results/evaluate_head_log.txt
 
 
 
 # Run the FINE training and evaluation scripts
-echo "=== TRAINING FINE ==="
-python train_fine.py | tee outputs/fine/results/train_fine_log.txt
+#echo "=== TRAINING FINE ==="
+#python train_fine.py | tee outputs/fine/results/train_fine_log.txt
 
-echo "=== POSTPROCESSING ==="
-python postprocessing.py | tee outputs/postprocessing_log.txt
+#echo "=== POSTPROCESSING ==="
+#python postprocessing.py | tee outputs/postprocessing_log.txt
 
 echo "=== EVALUATION FINE ==="
 python evaluate_fine.py | tee outputs/fine/results/evaluate_fine_log.txt
