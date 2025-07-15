9557114b6e66ed6e9ad40dd73db2d12420c053ea
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-15 09:40:37 +0100

    Ablation3-just air removal as preprocessing

--- 2_preprocess.py
+++ 2_preprocess.py
@@ -109,4 +109,4 @@ for key in input_folders:
         img_pil = Image.fromarray(img_rgb) #img_sharp.save(processed_path)
         img_pil.save(processed_path)       #img_sharp.save(processed_path)
 
-    print(f"{key.upper()}: {len(all_images)} images processed.")
+    print(f"{key.upper()}: {len(all_images)} images processed")
