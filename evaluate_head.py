160a5861ce48be9278e9308bcd4942919e9f81ca
RVasconcelosMiguel <rodrigomiguel02@gmail.com>
2025-07-20 13:23:51 +0100

    Ablation study - efficientnetB1

--- evaluate_head.py
+++ evaluate_head.py
@@ -12,8 +12,8 @@ from data_loader import get_generators
 from plot_utils import save_confusion_matrix, save_roc_curve
 
 # === CONFIGURATION ===
-model_name = "efficientnetb0"
-IMG_SIZE = 224
+model_name = "efficientnetb1"
+IMG_SIZE = 240
 BATCH_SIZE = 16
 THRESHOLD = 0.5
 
